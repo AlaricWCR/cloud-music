999922edf737b7411b5ed33c3d814417320cd973
wangchengrui <1872164629@qq.com>
2021-01-28 22:31:17 +0800

    云音乐小程序

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -35,61 +35,6 @@ Page({
     }
   ],
   playlist: []
-//   playlist: [{
-//     "id": "1001",
-//     "playCount": 1.4641238e+06,
-//     "name": "劲♂ 夫 ♀ 处 刑 曲",
-//     "picUrl": "http://p4.music.126.net/klQ-QH6R73Z7LdLkNvl78A==/109951165374086761.jpg?param=140y140"
-//   },
-//   {
-//     "id": "1002",
-//     "playCount": 622822.6,
-//     "name": "你一定要在自己热爱的世界里闪闪发亮啊",
-//     "picUrl": "http://p3.music.126.net/uesfHcJmZ23S3er_1mpeaw==/109951165621856219.jpg?param=140y140"
-//   },
-//   {
-//     "id": "1003",
-//     "playCount": 1.785097e+06,
-//     "name": "无论吃了多少瓜，依旧忠于自己热爱生活",
-//     "picUrl": "http://p2.music.126.net/oILIciXGa_33JWkk1MOtnA==/109951165650557478.jpg?param=140y140"
-//   },
-//   {
-//     "id": "1004",
-//     "playCount": 7.719329e+06,
-//     "name": "你明目张胆的偏爱就是救赎",
-//     "picUrl": "http://p3.music.126.net/7DthZlysMRwZY4_oG84VFQ==/109951165542744632.jpg?param=140y140"
-//   },
-//   {
-//     "id": "1005",
-//     "playCount": 1.06749088e+08,
-//     "name": "2021年全网最流行宝藏新歌",
-//     "picUrl": "http://p4.music.126.net/vMiQiZ1PRtkdacqJaruQVg==/109951165611860486.jpg?param=140y140"
-//   },
-//   {
-//     "id": "1006",
-//     "playCount": 1.5742008e+06,
-//     "name": "大赦天下 条子 蹦迪抖腿",
-//     "picUrl": "http://p4.music.126.net/RN8w_qrmZH8atCkGHmsKLg==/109951165474251886.jpg?param=140y140"
-//   },
-//   {
-//     "id": "1007",
-//     "playCount": 1.1042008e+06,
-//     "name": "想和你漫步雪中，听你哼唱我爱听的歌",
-//     "picUrl": "http://p4.music.126.net/q3Xt_5fuORGktCRTMrVjlQ==/109951165627341690.jpg?param=140y140"
-//   },
-//   {
-//     "id": "1008",
-//     "playCount": 1.3242008e+06,
-//     "name": "相遇，紧张，脸红，躲闪，自言自语",
-//     "picUrl": "http://p3.music.126.net/FGq0zEGlFRfumV8n4zRzQg==/109951165643217212.jpg?param=140y140"
-//   },
-//   {
-//     "id": "1009",
-//     "playCount": 1.0742008e+06,
-//     "name": "皮皮虾渡夕（BGM）",
-//     "picUrl": "http://p4.music.126.net/Y4yCHC_COlb4P1upACm4cQ==/109951165532510984.jpg?param=140y140"
-//   }
-// ]
 },
 
   /**
@@ -131,14 +76,17 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.setData({
+      playlist: []
+    })
+    this._getPlaylist()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this._getPlaylist()
   },
 
   /**
@@ -150,12 +98,18 @@ Page({
         title: '加载中',
       })
       wx.cloud.callFunction({
-        name: 'playlist'
+        name: 'music',
+        data: {
+          start : this.data.playlist.length,
+          count: MAX_LIMIT,
+          $url: 'playlist',
+        }
       }).then((res) => {
         console.log(res)
         this.setData({
-          playlist: res.result
+          playlist: this.data.playlist.concat(res.result.data)
         })
+        wx.stopPullDownRefresh()
         wx.hideLoading()
       })
   },
