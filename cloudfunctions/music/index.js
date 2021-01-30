91942034ad6cab588b313a0e8e370c55734c76a6
wangchengrui <1872164629@qq.com>
2021-01-30 19:13:00 +0800

    云音乐day07

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -10,7 +10,7 @@ const TcbRouter = require('tcb-router')
 // 引入axios
 const axios = require('axios')
 // 定义基础URL，修改你自己的穿透地址！！！
-const BASE_URL = 'https://wxwyy.cn.utools.club'
+const BASE_URL = 'http://47.98.169.198:3000'
 
 
 // 云函数入口函数
@@ -40,6 +40,7 @@ exports.main = async (event, context) => {
 
   //根据歌曲id获取歌曲播放的url
   app.router('musicUrl', async (ctx, next) => {
+    console.log(`${BASE_URL}/song/url?id=${event.musicId}`)
     const res = await axios.get(`${BASE_URL}/song/url?id=${event.musicId}`)
     ctx.body = res.data
   })
--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,26 +1,29 @@
 let musiclist = []
 // 正在播放的歌曲index
 let playingIndex = 0
+const backgroundAudioManager = wx.getBackgroundAudioManager()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    picUrl: ''
+    picUrl: '',
+    isPlaying:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    // console.log(options)
     console.log(options.musicId,typeof (options.musicId))
     playingIndex = options.index
     musiclist = wx.getStorageSync('musiclist')
     this._loadMusicDetail(options.musicId)
   },
 
+  
 
   _loadMusicDetail(musicId){
     let music = musiclist[playingIndex]
@@ -39,8 +42,53 @@ Page({
         $url: 'musicUrl'
       }
     }).then((res) => {
-      console.log(res)        
+      console.log(res)
+      const url = res.result.data[0].url
+      if(url === null){
+        wx.showToast({
+          title: '没有权限播放',
+        })
+        backgroundAudioManager.pause()
+        this.setData({
+          isPlaying: false
+        })
+        return
+      }    
+      backgroundAudioManager.src = url
+      backgroundAudioManager.title = music.name
+      backgroundAudioManager.coverImgUrl = music.al.picUrl
+      backgroundAudioManager.singer = music.ar[0].name
+      this.setData({
+        isPlaying: true
+      })
     })
+  },
+
+  togglePlaying(){
+    if(this.data.isPlaying){
+      backgroundAudioManager.pause()
+    }else{
+      backgroundAudioManager.play()
+    }
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
+  },
+
+  onPrev(){
+    playingIndex--
+    if(playingIndex === musiclist.length){
+      playingIndex = musiclist.length - 1
+    }
+    this._loadMusicDetail(musiclist[playingIndex].id)
+  },
+
+  onNext(){
+    playingIndex++
+    if(playingIndex === musiclist.length){
+      playingIndex = 0
+    }
+    this._loadMusicDetail(musiclist[playingIndex].id)
   },
 
   /**
