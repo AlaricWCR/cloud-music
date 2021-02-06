5b05bb0fca531f7af4d99f025f0c70b352aeb2c8
wangchengrui <1872164629@qq.com>
2021-02-06 23:04:24 +0800

    云音乐11

--- miniprogram/components/search/search.js
+++ miniprogram/components/search/search.js
@@ -1,4 +1,4 @@
-// components/search/search.js
+let keyword = ''
 Component({
   /**
    * 组件的属性列表
--- miniprogram/pages/find/find.js
+++ miniprogram/pages/find/find.js
@@ -1,22 +1,55 @@
-// pages/find/find.js
+let keyword = '' //搜索关键字
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    onSearch(event) {
+      // 控制底部弹出层是否显示
+      modalShow: false,
+  },
+  onSearch(event) {
       keyword = event.detail.keyword
       console.log(keyword)
     },
     onLoad(options) {},
     onPublish() {
+      // 获取用户当前设置。返回值中只会出现小程序已经向用户请求过的权限，
+      // 根据是否具有scope。userInfo属性，判断用户是否授权
+      wx.getSetting({
+        success: (res) => {
+          console.log('当前设置' + JSON.stringify(res))
+          if (res.authSetting['scope.address.userInfo']) {
+            wx.getUserInfo({
+              success: (res) => {
+                console.log(res)
+                this.onLoginSuccess({
+                  detail: res.userInfo
+                })
+              }
+            })
+          } else {
+            this.setData({
+              modalShow: true,
+            })
+          }
+        }
+      })
+    },
+    onLoginSuccess(event) {
+      console.log('>>>>>' + event)
+      const detail = event.detail
+      console.log(detail.nickName)
       wx.navigateTo({
         url: '../publish/publish',
       })
     },
-  },
-
+    onLoginFail() {
+      wx.showModal({
+        title: '授权用户才能发布',
+        content: '',
+      })
+    },
   /**
    * 生命周期函数--监听页面加载
    */
